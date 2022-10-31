17d76151188b1ee7ba6381a04faaa0d48c0c4e5d
Andrey Kozin <a.kozin@nemo.tavel>
2022-10-31 22:30:36 +0300

    added new test + readable assert

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,7 +1,18 @@
+from selenium.common import NoSuchElementException
+
+
 class BasePage:
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
+
+    def is_element_present(self, locator, locator_value):
+        try:
+            self.browser.find_element(locator, locator_value)
+        except NoSuchElementException:
+            return False
+        return True
